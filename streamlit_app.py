343e4fef27862cddc5b6e79a0109bc3d24d55f42
serbel15706 <serbel15706@gmail.com>
2024-05-22 23:14:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -179,7 +179,7 @@ def vectorize_text(uploaded_files):
 
                 docs = []
                 loader = JSONLoader(temp_filepath, jq_schema='.')
-                docs.extend(loader.load()
+                docs.extend(loader.load())
                  text_splitter = RecursiveCharacterTextSplitter(
                     chunk_size = 1500,
                     chunk_overlap  = 100
